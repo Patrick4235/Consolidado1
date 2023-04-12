f8da66ae9424b86c244c2d0b5c87b36d6157748c
Pat <72098954@continental.edu.pe>
2023-04-12 16:54:33 -0500

    Arreglando bugs

--- codigo.py
+++ codigo.py
@@ -14,35 +14,27 @@ def divicion(a,b):
     resultado=a/b
     return resultado
 
-Num1 = float(input("ingrese el primer numero: "))
-Num2 = float(input("ingrese el primer numero: "))
+try: 
+    Num1 = float(input("ingrese el primer numero: "))
+    Num2 = float(input("ingrese el primer numero: "))
+except ValueError:
+        print("SOLO SE ACEPTAN NUMEROS")
 
 
 
 operacion = int (input("escriba el numero de la operacion a realisar: \n 1.- suma \n 2.- Resta \n 3.- multiplicacion \n 4.- divicion \n Coloque numero: "))
 
 if operacion == 1:
-    try:
-        print(f"el resultado es: {suma(Num1,Num2)}")
-    except ValueError:
-        print("SOLO SE ACEPTAN NUMEROS")
+    print(f"el resultado es: {suma(Num1,Num2)}")
         
 elif operacion == 2:
-    try:
-        print(f"el resultado es: {resta(Num1,Num2)}")
-    except ValueError:
-        print("SOLO SE ACEPTAN NUMEROS")
+    print(f"el resultado es: {resta(Num1,Num2)}")
     
 elif operacion == 3:
-    try:
-        print(f"el resultado es: {multiplicacion(Num1,Num2)}")
-    except ValueError:
-        print("SOLO SE ACEPTAN NUMEROS")
+    print(f"el resultado es: {multiplicacion(Num1,Num2)}")
     
 elif operacion == 4:
     try:
         print(f"el resultado es: {divicion(Num1,Num2)}")
     except ZeroDivisionError:
         print("NO SE PUEDE DIVIDIR POR CERO")
-    except ValueError:
-        print("SOLO SE ACEPTAN NUMEROS")
