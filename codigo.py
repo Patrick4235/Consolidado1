e244123e4968e15d5ada3a2b16fd145cdcc104c9
Pat <72098954@continental.edu.pe>
2023-04-12 16:58:31 -0500

    Arreglando bugs del master

--- codigo.py
+++ codigo.py
@@ -17,24 +17,25 @@ def divicion(a,b):
 try: 
     Num1 = float(input("ingrese el primer numero: "))
     Num2 = float(input("ingrese el primer numero: "))
+
+    operacion = int (input("escriba el numero de la operacion a realisar: \n 1.- suma \n 2.- Resta \n 3.- multiplicacion \n 4.- divicion \n Coloque numero: "))
+
+    if operacion == 1:
+        print(f"el resultado es: {suma(Num1,Num2)}")
+        
+    elif operacion == 2:
+        print(f"el resultado es: {resta(Num1,Num2)}")
+    
+    elif operacion == 3:
+        print(f"el resultado es: {multiplicacion(Num1,Num2)}")
+    
+    elif operacion == 4:
+            print(f"el resultado es: {divicion(Num1,Num2)}")
 except ValueError:
         print("SOLO SE ACEPTAN NUMEROS")
+except ZeroDivisionError:
+      print("NO SE PUEDE DIVIDIR POR CERO")
 
 
 
-operacion = int (input("escriba el numero de la operacion a realisar: \n 1.- suma \n 2.- Resta \n 3.- multiplicacion \n 4.- divicion \n Coloque numero: "))
 
-if operacion == 1:
-    print(f"el resultado es: {suma(Num1,Num2)}")
-        
-elif operacion == 2:
-    print(f"el resultado es: {resta(Num1,Num2)}")
-    
-elif operacion == 3:
-    print(f"el resultado es: {multiplicacion(Num1,Num2)}")
-    
-elif operacion == 4:
-    try:
-        print(f"el resultado es: {divicion(Num1,Num2)}")
-    except ZeroDivisionError:
-        print("NO SE PUEDE DIVIDIR POR CERO")
