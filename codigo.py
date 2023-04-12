bed2743a30af4167303d002d390818cac50b90cc
Edward <74061627@continental.edu.pe>
2023-04-12 16:32:19 -0500

    coreccion de fallos de ejecucion

--- codigo.py
+++ codigo.py
@@ -1,33 +1,33 @@
 def suma(a,b):
     resultado=a+b
-    return print("el resultado es: "+resultado)
+    return resultado
 
 def resta(a,b):
     resultado=a-b
-    return print("el resultado es: "+resultado)
+    return resultado
 
 def multiplicacion(a,b):
     resultado=a*b
-    return print("el resultado es: "+resultado)
+    return resultado
 
-def divicion(a/b):
-    resultado=a+b
-    return print("el resultado es: "+resultado)
+def divicion(a,b):
+    resultado=a/b
+    return resultado
 
-Num1 = input("ingrese el primer numero: ")
-Num2 = input("ingrese el primer numero: ")
+Num1 = float(input("ingrese el primer numero: "))
+Num2 = float(input("ingrese el primer numero: "))
 
-operacion = input("escriba el numero de la operacion a realisar: \n 1.- suma \n 2.- Resta \n 3.- multiplicacion \n 4.- divicion \n Coloque numero: ")
+operacion = int (input("escriba el numero de la operacion a realisar: \n 1.- suma \n 2.- Resta \n 3.- multiplicacion \n 4.- divicion \n Coloque numero: "))
 
 if operacion == 1:
-    suma(Num1,Num2)
+    print(f"el resultado es: {suma(Num1,Num2)}")
+    
 
-elif operacion ==2:
-    resta(Num1,Num2)
+elif operacion == 2:
+    print(f"el resultado es: {resta(Num1,Num2)}")
     
-elif operacion ==3:
-    multiplicacion(Num1,Num2)
+elif operacion == 3:
+    print(f"el resultado es: {multiplicacion(Num1,Num2)}")
     
-elif operacion ==4:
-    divicion(Num1,Num2)
-    
+elif operacion == 4:
+    print(f"el resultado es: {divicion(Num1,Num2)}")
